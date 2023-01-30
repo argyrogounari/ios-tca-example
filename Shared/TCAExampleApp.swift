54531e121bf3be0cdaa46b25e42f3e9e3e0b8248
argyro gounari <agounari@trading-point.com>
2023-01-30 09:37:07 +0200

    Initial Commit

---
+++ Shared/TCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExampleApp.swift
+//  Shared
+//
+//  Created by Argyro Gounari on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
